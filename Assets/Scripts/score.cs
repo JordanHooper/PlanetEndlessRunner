7eac2cfb6c849057f72032aee0484cacf4f1b4de
JordanHooper <jordan.hooper@hotmail.co.uk>
2017-02-06 17:25:02 +0000

    blah

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public Text myText;
+    float timing = 0f;
+    int modifier = 10;
+
+    void Update()
+    {
+        currentScore += (Time.deltaTime * modifier);
+
+        myText.text = "Current score: " + timing("f2");
+    }
+
+}
